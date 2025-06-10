30ebe7158b60c5ba59861db85992aa4989e0ea89
dcherdyntsev <chdvl@yandex.ru>
2025-06-10 19:50:12 +0300

    message

--- test_get_async.py
+++ test_get_async.py
@@ -4,6 +4,7 @@ import time
 import grequests
 import hh
 import asyncio
+import threading
 
 ids=['120236562','120877600', '120776613', '120072589']
 ids.extend(ids*2)
@@ -11,6 +12,14 @@ ids.extend(ids*2)
 def sync_get():
     for _id in ids:
         res = hh.get_vacancy_desc(_id)
+
+def thread_get():
+    threads = []
+    for _id in ids:
+        t=threading.Thread(target=hh.get_vacancy_desc, args=(_id,))
+        t.start()
+        threads.append(t)
+    for t in threads: t.join()
 
 async def async_get():
     for _id in ids:
@@ -32,3 +41,4 @@ if __name__ == '__main__':
     test_time(g_requests)
     test_time(sync_get)
     test_time(asyncio_get)
+    test_time(thread_get)
